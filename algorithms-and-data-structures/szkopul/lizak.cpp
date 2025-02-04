4d982464a8f568286ff652d9fede280b86bf412f
Michał Zientek <94467965+AveePB@users.noreply.github.com>
2025-02-04 12:45:53 +0100

    lizak.cpp: replaced ds with arrays

--- algorithms-and-data-structures/szkopul/lizak.cpp
+++ algorithms-and-data-structures/szkopul/lizak.cpp
@@ -2,16 +2,19 @@
 #include <vector>
 #include <unordered_map>
 
+const int MAX_VAL = 2e6 + 7;
+const int MAX_SIZE = 1e6 + 7;
+
 /**
  * @breif Finds the max sum of the range with reversed parity of the total sum.
- * @param v the vector of numbers.
+ * @param arr the array of numbers.
+ * @param size the size of an array.
  * @param totalSum the sum of all elements of v.
  * @param lPtr the left pointer of reversed range.
  * @param rPtr the righ pointer of reversed range.
  * @param sum the sum of reversed range.
 */
-void findMaxRevParityRange(std::vector<int>& v, int totalSum, int& lPtr, int& rPtr, int& sum);
-
+void findMaxRevParityRange(int* arr, int size, int totalSum, int& lPtr, int& rPtr, int& sum);
 
 /**
  * @brief Creates all possible numbers from v (in given parity).
@@ -21,7 +24,7 @@ void findMaxRevParityRange(std::vector<int>& v, int totalSum, int& lPtr, int& rP
  * @param r the right pointer.
  * @param sum the current sum.
 */
-void remember(std::unordered_map<int, std::pair<int, int>>& m, std::vector<int>& v, int l, int r, int sum);
+void remember(std::pair<int, int>* ans, int* arr, int l, int r, int sum);
 
 // https://szkopul.edu.pl/problemset/problem/AWhdD7i4V7mupdKWVtpgfGSM/site/?key=statement
 int main() {
@@ -30,46 +33,48 @@ int main() {
     std::cout.tie(0);
     std::cin.tie(0);
 
-    // Initialize variables and DS
-    int k, nSegments, nQueries, totalSum = 0;
+    // Initialize variables
+    int k, nSegments, nQueries, totalSum = 0, arr[MAX_SIZE];
     char el;
+
+    // Initialize data structure
+    std::pair<int, int> ans[MAX_VAL];
+    for (int i = 0; i < MAX_VAL; i++) ans[i] = { -1, -1 };
 
     // Input
     std::cin >> nSegments >> nQueries;
-    std::vector<int> vec(nSegments+1, 0);
-    vec[0] = -1;
+    arr[0] = -1;
 
     for (int i = 1; i <= nSegments; i++) {
         std::cin >> el;
         // Strawberry (2)
         if (el == 'T') {
             totalSum += 2;
-            vec[i] = 2;
+            arr[i] = 2;
         }
 
         // Vanilla (1)
         else {
             totalSum += 1;
-            vec[i] = 1;
+            arr[i] = 1;
         }
     }
 
     // Find rev parity range
     int l = -1, r = -1, sum = -1;
-    findMaxRevParityRange(vec, totalSum, l, r, sum);
+    findMaxRevParityRange(arr, nSegments+1, totalSum, l, r, sum);
 
     // Discover all possible numbers
-    std::unordered_map<int, std::pair<int, int>> ans;
     
-    remember(ans, vec, 1, nSegments, totalSum);
-    if (l != -1 && r != -1 && sum > 0) remember(ans, vec, l, r, sum);
+    remember(ans, arr, 1, nSegments, totalSum);
+    if (l != -1 && r != -1 && sum > 0) remember(ans, arr, l, r, sum);
 
     // Handle all queries
     for (int i = 0; i < nQueries; i++) {
         std::cin >> k;
         
         // Segment wasn't found
-        if (ans.find(k) == ans.end()) {
+        if (ans[k].first == -1 || ans[k].second == -1) {
             std::cout << "NIE\n";
         }
 
@@ -82,32 +87,32 @@ int main() {
     return 0;
 }
 
-void findMaxRevParityRange(std::vector<int>& v, int totalSum, int& lPtr, int& rPtr, int& sum) {
+void findMaxRevParityRange(int* arr, int size, int totalSum, int& lPtr, int& rPtr, int& sum) {
     // Go from left
     int l = -1, Lsum = 0;
-    for (int i = 1; i < v.size(); i++) {
+    for (int i = 1; i < size; i++) {
         // Element is Odd
-        if (v[i] == 1) {
+        if (arr[i] == 1) {
             Lsum += totalSum - 1;
             l = i + 1;
             break;
         }
         else {
-            Lsum -= v[i];
+            Lsum -= arr[i];
         }
     }
 
     // Go from right
     int r = -1, Rsum = 0;
-    for (int i = v.size() - 1; i >= 1; i--) {
+    for (int i = size - 1; i >= 1; i--) {
         // Element is Odd
-        if (v[i] == 1) {
+        if (arr[i] == 1) {
             Rsum += totalSum - 1;
             r = i - 1;
             break;
         }
         else {
-            Rsum -= v[i];
+            Rsum -= arr[i];
         }
     }
 
@@ -118,24 +123,24 @@ void findMaxRevParityRange(std::vector<int>& v, int totalSum, int& lPtr, int& rP
     }
     else {
         lPtr = l;
-        rPtr = v.size() - 1;
+        rPtr = size - 1;
         sum = Lsum;
     }
 }
 
-void remember(std::unordered_map<int, std::pair<int, int>>& m, std::vector<int>& v, int l, int r, int sum) {
+void remember(std::pair<int, int>* ans, int* arr, int l, int r, int sum) {
     // Go through vector
     while (l <= r) {
         // Save answer
-        m[sum] = { l, r };
+        ans[sum] = { l, r };
 
         // Left is even
-        if (v[l] == 2) {
+        if (arr[l] == 2) {
             l += 1;
         }
 
         // Right is even
-        else if (v[r] == 2) {
+        else if (arr[r] == 2) {
             r -= 1;
         }
 
